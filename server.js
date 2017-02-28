598010e278e75bd5e50e6a0c261d925d06443d26
Nilesh-mindstix <nilesh.pharande@mindstix.com>
2017-02-28 17:05:02 +0530

    first commit.

---
+++ server.js
@@ -0,0 +1,49 @@
+// http://localhost:3000/convert/csv/to/json?q=https://s3.amazonaws.com/ed-college-choice-public/Most+Recent+Cohorts+(Scorecard+Elements).csv
+// https://www.npmjs.com/package/csvtojson
+// https://www.npmjs.com/package/request
+// http://expressjs.com/en/guide/routing.html
+// https://www.tutorialspoint.com/nodejs/nodejs_streams.htm
+// http://codewinds.com/blog/2013-08-02-streams-basics.html
+// how to run nodejs app on heroku  --->  https://devcenter.heroku.com/articles/deploying-nodejs
+
+
+var express = require("express");
+var request = require('request');
+var csvToJson = require("csvtojson");
+//var bodyParser = require("body-parser");
+var app = express();
+var port = 3000;
+try {
+	app.get("/convert/csv/to/json", function(req, res) {
+		console.log("STRT_TIME: ", new Date());
+		if(!req.query.q) {
+			console.log("not received any file name in request.");
+			res.end("ERROR: not received any file name.");
+		}
+		request(req.query.q, function (error, response, body) {
+			console.log("RCVD_MSGE: ", new Date());
+			if (!error && response.statusCode == 200) {
+				csvToJson()
+				.fromString(body)
+		        .on('json',function(json) {
+				    res.write(JSON.stringify(json));
+				    //console.log("SEND_MSGE: ", new Date());
+			    }).on('done',function() {
+				    console.log("END__MSGE: ", new Date());
+			        res.end("Hello world: "+ count);
+			    });
+			}
+		});
+	});
+	app.get("/", function(req, res) {
+		res.end("Hello from Rooot.");
+	});
+
+	app.listen(port, function listener(err) {
+		if(err) console.log("err: ", err);
+		console.log("Server is listening on port: ", port);
+	});
+} catch(error) {
+	//Code terminated abnormally.
+	console.log("Program terminated.");
+}
