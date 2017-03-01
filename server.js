0cc28ab1f9e0cd9d3ea88c4cdc7e4e34d0fa64f0
NileshPharande <nileshpharande88@gmail.com>
2017-03-01 11:38:08 +0530

    Adding winston as the logger

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var request = require('request');
 var csvToJson = require("csvtojson");
+var winston = require("winston");
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,21 +10,20 @@ var port = process.env.PORT || 3000;
 app.get("/convert/csv/to/json", function(req, res) {
 
 	// Log for getting start tme of the request execution.
-	console.log("start_time: ", new Date());
+	winston.log("start_time: ", new Date());
 	
 	// if request not containing link for file in it return error message
 	var queryString = req.query.q;
 	if(!queryString) {
-		console.log("not received any file name in request.");
-		res.status(400).send("Please mention file name in URL");
+		winston.error("Query not recived");
+		res.status(400).json({"message":"Kindly send file URL and try again"});
 	}
 
 	// Make request for file from received url.
 	request(queryString, function requetHandler(error, response, body) {
 		
-		console.log("data_received_time:- ", new Date());
-		console.log("status_code:- ", response.statusCode);
-		console.log("content-type:- ", response.headers['content-type'])
+		winston.log("data_received_time:- ", new Date());
+		winston.info("status_code:- ", response.statusCode);
 		
 		//check for received data from url or not.
 		if (!error && response.statusCode == 200) {
@@ -35,29 +35,29 @@ app.get("/convert/csv/to/json", function(req, res) {
 		        .on('json', function jsonForSingleRecord(json) {
 				    res.write(JSON.stringify(json));
 			    }).on('done', function conversionComplete() {
-				    console.log("end_time: ", new Date());
+				    winston.log("end_time: ", new Date());
 			        res.status(200).end();
 			    });
 			} else {
 				//Return error as file is not csv file.
-				console.log("Received file is not a .csv");
-				res.status(400).send("Received file from url is not csv");
+				winston.error("Received file is not a .csv");
+				res.status(400).json({"message":"Received file from url is not csv"});
 			}
 		} else {
 			//If failed to read file contents from the path
-			console.log("file not found at path:- ", queryString);
-			console.log("file not found Error:- ", error);
-			res.status(response.statusCode).send("FILE ACCESS FAILURE.");
+			winston.error("file not found at path:- ", queryString);
+			winston.error("file not found Error:- ", error);
+			res.status(response.statusCode).json({"message":"FILE ACCESS FAILURE"});
 		}
 	});
 });
 // API fpr root request.
 app.get("/", function(req, res) {
-	res.end("Hello from Rooot.");
+	res.end({"message":"Application is working, all set to use"});
 });
 
 // server start listening on available port.
 app.listen(port, function listener(err) {
-	if(err) console.log("err: ", err);
-	console.log("Server is listening on port: ", port);
+	if(err) winston.log("err: ", err);
+	winston.info("Server is listening on port: ", port);
 });
